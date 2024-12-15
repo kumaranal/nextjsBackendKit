a8a6aa5dad4690c3c706e7802de00e3c782ed8e3
kumaranal <analbiswas180@gmail.com>
2024-12-15 14:11:06 +0530

    initial commit

--- src/app/api/util/swagger.ts
+++ src/app/api/util/swagger.ts
@@ -1,25 +1,19 @@
-import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerJsDoc from "swagger-jsdoc";
 const swaggerOptions = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'API Documentation',
-      version: '1.0.0',
-      description: 'API documentation for my Next.js application',
+      title: "API Documentation",
+      version: "1.0.0",
+      description: "API documentation for my Next.js application",
     },
     servers: [
       {
-        url: 'http://localhost:3000/api',
-      },
-      {
-        url: 'http://{{changeable}}',
-      },
-      {
-        url: 'http://localhost:3000',
+        url: process.env.NEXT_PUBLIC_SITE_URL,
       },
     ],
   },
-  apis: ['./src/app/api/**/*.ts'], // Path to your API route files
+  apis: ["./src/app/api/**/*.ts"], // Path to your API route files
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
