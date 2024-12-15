0993e057b6d991ca674934f617db4c6b8147203b
kumaranal <analbiswas180@gmail.com>
2024-12-15 14:26:59 +0530

    initial commit

--- src/app/api/swaggerJsonData/route.ts
+++ src/app/api/swaggerJsonData/route.ts
@@ -2,6 +2,9 @@ import swaggerDocs from "../util/swagger";
 
 export async function GET(request: Request) {
   return new Response(JSON.stringify(swaggerDocs), {
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
   });
 }
